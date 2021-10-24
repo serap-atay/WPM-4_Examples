599a722ba6aa4db5f661244be6221b28c4ee45ed
atays <atays@LAPTOP-2FFKP26U>
2021-10-24 23:56:56 +0300

    Add project files.

---
+++ BomOyunu/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace BomOyunu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //SORU-1 5 ve 5'in katları bom
+            //for (int i = 1; i <= 100; i++)
+            //{
+            //    if(i%5==0)
+            //    {
+            //        Console.WriteLine("BOM");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+            //SORU-2 Bom sayısı kullanıcı tarafından girilen Bom
+            //Console.WriteLine("BOM sayisini giriniz: ");
+            //int sayi = Convert.ToInt32(Console.ReadLine());
+            //for (int i = 1; i < =100; i++)
+            //{
+            //    if (i%sayi==0)
+            //    {
+            //        Console.WriteLine("BOM");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+            //SORU-3 BOM sayisi kulanıcı girişli oyunu bilgisayarın başlatması
+            Console.WriteLine("BOM sayisi giriniz: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    if (i % sayi == 0)
+                    {
+                        Console.WriteLine("BOM");
+                    }
+                    else
+                    {
+                        Console.WriteLine(i);
+                    }
+
+                }
+                else
+                {
+
+                    Console.WriteLine("Sıra sizde : ");
+                    if (i % sayi == 0)
+                    {
+                        if("BOM"==Console.ReadLine() || "bom" == Console.ReadLine())
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Oyunu kaybettiniz.");
+                            break;
+                        }
+
+                    }
+                    else
+                    {
+                        if (i==Convert.ToInt32( Console.ReadLine()))
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Oyunu kaybettiniz.");
+                            break;
+                        }
+                    }
+
+                
+                }
+            }
+            Console.WriteLine("Oyun berabere.");
+
+        }
+
+    }
+}
+
+
---
+++ Consoleörneksoruveçözümler/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Consoleörneksoruveçözümler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // SORU-1 girilen 10 sayının çift mi tek mi olduğunu bulan program
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine("Bir sayi giriniz: ");
+            //    int sayi = Convert.ToInt32(Console.ReadLine());
+            //    if (sayi % 2 == 0)
+            //    {
+            //        Console.WriteLine(sayi + " sayisi çifttir.");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(sayi + "sayisi tektir.");
+            //    }
+            //}
+
+            //SORU-2 girilen 5 sayının toplamını bulan program
+            //int toplam = 0;
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine("bir sayi giriniz:");
+            //    int sayi = Convert.ToInt32(Console.ReadLine());
+            //    toplam += sayi;
+            //}
+            //Console.WriteLine("Toplam :"+ toplam);
+
+            //SORU-3 girilen sayının asal olup olmadığını bulma
+            Console.WriteLine("Bir sayi giriniz:");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            if (sayi == 2)
+            {
+                Console.WriteLine(sayi + " sayisi asal sayıdır.");
+            }
+            else
+            {
+                 for (int i = 2; i < sayi; i++)
+                {
+                    if (sayi%i==0)
+                    {
+                        Console.WriteLine(sayi+"sayisi asal değildir.");
+                        break;
+                    }
+                    else
+                    {
+
+                        if (sayi == i+1)
+                        {
+                            Console.WriteLine(sayi +"sayisi asaldır.");
+                        }
+                        continue;
+                    }
+                             
+                }
+
+               
+            }
+          
+           
+        }
+    }
+}
